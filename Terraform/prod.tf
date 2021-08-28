08bf0871f184af2064898cb76f86a5ec39497117
Priyanshu Goel <goelg.goel@gmail.com>
2021-08-28 13:06:36 +0530

    Defined Initial Web Security Group

--- Terraform/prod.tf
+++ Terraform/prod.tf
@@ -1,11 +1,41 @@
 provider "aws" {
   profile = "default"
-  region = "us-east-1"
+  region  = "us-east-1"
 }
 
 resource "aws_s3_bucket" "prod_tf_course" {
-  bucket = "tf-course-20210821"
-  acl = "private"
+  bucket = "tf-course-20210830"
+  acl    = "private"
 }
 
 resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "prod_web" {
+  name        = "prod_web"
+  description = "Allow standard http and https ports inbound and everything outbounds"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    "Terraform" = "true"
+  }
+
+}
