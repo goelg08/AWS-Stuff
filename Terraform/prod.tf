bc5161836cdddc0546f1ff28b461fe277ea9400f
Priyanshu Goel <goelg.goel@gmail.com>
2021-08-28 11:53:34 +0530

    Terraform Changes

---
+++ Terraform/prod.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "prod_tf_course" {
+  bucket = "tf-course-20210821"
+  acl = "private"
+}
+
+resource "aws_default_vpc" "default" {}
